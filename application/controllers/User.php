34e7ac7e49d40eb85c890e7f465a6af067c14921
Ghs267 <hariyantosetiadi@gmail.com>
2020-05-12 07:13:50 +0700

    add edit profile method

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -22,4 +22,59 @@ class User extends CI_Controller
         $this->load->view('user/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function edit()
+    {
+        $data['title'] = 'Edit Profile';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('fname', 'First Name', 'required|trim');
+        $this->form_validation->set_rules('address', 'Address', 'required|trim');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $fname = $this->input->post('fname');
+            $lname = $this->input->post('lname');
+            $email = $this->input->post('email');
+            $alamat = $this->input->post('address');
+
+            // cek jika ada gambar
+            $upload = $_FILES['image']['name'];
+
+            if ($upload) {
+                $config['upload_path'] = './assets/img/profile/';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '2048';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old = $data['user']['image'];
+                    if ($old != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old);
+                    }
+                    $new_foto = $this->upload->data('file_name');
+                    $this->db->set('image', $new_foto);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->set('first_name', $fname);
+            $this->db->set('last_name', $lname);
+            $this->db->set('alamat', $alamat);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Your profile has been updated!!</div>');
+            redirect('user');
+        }
+    }
 }
