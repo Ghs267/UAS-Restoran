6ad63c5acf49feb3745c827112f08ef6b86a2bb2
Ghs267 <hariyantosetiadi@gmail.com>
2020-05-14 09:31:36 +0700

    add view table in shop

--- application/views/food/shop.php
+++ application/views/food/shop.php
@@ -3,6 +3,17 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <table class="table table-hover">
+        <thead>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">QTY</th>
+            <th scope="col">Price</th>
+        <tbody>
+
+        </tbody>
+
+    </table>
 
 
 </div>
