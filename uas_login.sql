1fcb7585f8e29685d8627e78fed2c306572d7577
Ghs267 <hariyantosetiadi@gmail.com>
2020-05-14 11:36:39 +0700

    update new database

--- uas_login.sql
+++ uas_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Bulan Mei 2020 pada 13.30
+-- Waktu pembuatan: 14 Bulan Mei 2020 pada 06.36
 -- Versi server: 10.4.6-MariaDB
 -- Versi PHP: 7.3.8
 
@@ -38,52 +38,8 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `category`) VALUES
-(1, 'Main Course'),
-(2, 'Snack');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `category_d`
---
-
-CREATE TABLE `category_d` (
-  `id` int(11) NOT NULL,
-  `category` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `category_d`
---
-
-INSERT INTO `category_d` (`id`, `category`) VALUES
-(1, 'Soft Drink'),
-(2, 'Juice');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `drink`
---
-
-CREATE TABLE `drink` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `category` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `stock` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `drink`
---
-
-INSERT INTO `drink` (`id`, `name`, `category`, `price`, `stock`) VALUES
-(1, 'Jus ALPUKAT', 0, 12000, 12),
-(2, 'Soda Gembira', 1, 18000, 12),
-(3, 'Jus ALPUKAT', 2, 12000, 12),
-(5, 'Jus Kedondong', 2, 12000, 12),
-(6, 'Fresh milk boba', 1, 35000, 12);
+(1, 'Food'),
+(2, 'Drink');
 
 -- --------------------------------------------------------
 
@@ -105,10 +61,12 @@ CREATE TABLE `food` (
 
 INSERT INTO `food` (`id`, `name`, `category`, `price`, `stock`) VALUES
 (5, 'Ayam Geprek', '1', 18000, 20),
-(9, 'Babi Goreng', '1', 35000, 12),
-(10, 'Nasi Goreng Babi', '1', 35000, 12),
+(9, 'Babi Goreng', '1', 34000, 10),
+(10, 'Nasi Goreng Babi', '1', 35000, 8),
 (11, 'Kwetiau siram sapi', '1', 35000, 35),
-(12, 'Siomay (5pcs)', '2', 25000, 12);
+(12, 'Siomay (5pcs)', '1', 25000, 12),
+(13, 'Jus Mangga', '2', 15000, 1),
+(14, 'Jus Kedondong', '2', 15000, 2);
 
 -- --------------------------------------------------------
 
@@ -148,8 +106,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `tgl_lahir`, `alamat`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(4, 'Gregorius Hariyanto', 'Setiadi', '2000-07-26', 'UMN', 'hariyantosetiadi@gmail.com', '1565776685024.jpg', '$2y$10$lJKebrEnm.NcvFgEl.yCMu1z1dl0.ZyXr60rbyUfu3r1fOroYFeby', 1, 1, 1589164612),
-(5, 'Kevin Natanel', 'Hendrawan', '2009-01-15', 'konoha', 'hari@student.umn.ac.id', '6722540-m.jpg', '$2y$10$VlMwwpHZLC4f8F/ngr9LhuAHJuPbcsz96tlgGta.9UbgfoliRDx9q', 2, 1, 1589169467);
+(4, 'Gregorius Hariyanto', 'Setiadi', '2000-07-26', 'Tamci', 'hariyantosetiadi@gmail.com', 'line_73188113827642.jpg', '$2y$10$lJKebrEnm.NcvFgEl.yCMu1z1dl0.ZyXr60rbyUfu3r1fOroYFeby', 1, 1, 1589164612),
+(5, 'Kevin Natanel', 'Hendrawan', '2009-01-15', 'konoha', 'hari@student.umn.ac.id', '6722540-m.jpg', '$2y$10$VlMwwpHZLC4f8F/ngr9LhuAHJuPbcsz96tlgGta.9UbgfoliRDx9q', 2, 1, 1589169467),
+(6, 'Mickey', 'Richardo', '2020-05-05', 'amrik', 'mickey@gmail.com', '3840x2160-pebble-stone-sea-coast-4k_1551643576.jpg', '$2y$10$QR5mDO78PcNZwa43G0B8U.AHXA1EVC5mUtpiO1sTEBPDvUIsv0xai', 2, 1, 1589369717);
 
 -- --------------------------------------------------------
 
@@ -174,7 +133,10 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (13, 2, 8),
 (14, 1, 8),
 (15, 1, 9),
-(16, 2, 9);
+(16, 2, 9),
+(22, 1, 10),
+(23, 1, 11),
+(28, 2, 11);
 
 -- --------------------------------------------------------
 
@@ -195,7 +157,7 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 (1, 'Admin'),
 (2, 'User'),
 (3, 'Menu'),
-(9, 'Food');
+(11, 'Food');
 
 -- --------------------------------------------------------
 
@@ -242,12 +204,12 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (7, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
 (8, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
 (10, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
-(16, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
-(20, 9, 'Food Menu', 'food', 'fas fa-fw fa-utensils', 1),
-(22, 1, 'Food Management', 'admin/food', 'fas fa-fw fa-utensils', 1),
-(24, 1, 'Drink Management', 'admin/drink', 'fas fa-fw fa-glass-cheers', 1),
-(25, 9, 'Drink Menu', 'food/drink', 'fas fa-fw fa-glass-cheers', 1),
-(26, 9, 'Shopping Cart', 'food/shop', 'fas fa-shopping-cart', 1);
+(20, 11, 'Food and Drink Menu', 'food', 'fas fa-fw fa-utensils', 1),
+(22, 1, 'Food and Drink Management', 'admin/food', 'fas fa-fw fa-utensils', 1),
+(26, 11, 'Shopping Cart', 'food/shop', 'fas fa-fw fa-shopping-cart', 1),
+(27, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-edit', 1),
+(28, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
+(29, 1, 'History of Selling', 'admin/history', 'fas fa-history fa-fw', 1);
 
 --
 -- Indexes for dumped tables
@@ -257,18 +219,6 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 -- Indeks untuk tabel `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `category_d`
---
-ALTER TABLE `category_d`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `drink`
---
-ALTER TABLE `drink`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -324,22 +274,10 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `category_d`
---
-ALTER TABLE `category_d`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `drink`
---
-ALTER TABLE `drink`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT untuk tabel `food`
 --
 ALTER TABLE `food`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `shop_cart`
@@ -351,19 +289,19 @@ ALTER TABLE `shop_cart`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_role`
@@ -375,7 +313,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT untuk tabel `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
