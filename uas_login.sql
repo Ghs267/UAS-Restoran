a0a493d4139cc3e652c32f1165314419745cadb7
jennieflorensia <54789197+jennieflorensia@users.noreply.github.com>
2020-05-17 07:08:56 +0700

    database
    update database (uas_login.sql)

--- uas_login.sql
+++ uas_login.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Bulan Mei 2020 pada 06.36
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.3.8
+-- Generation Time: May 17, 2020 at 02:08 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -34,7 +34,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `category`) VALUES
@@ -44,47 +44,56 @@ INSERT INTO `category` (`id`, `category`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `food`
+-- Table structure for table `food`
 --
 
 CREATE TABLE `food` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `category` varchar(128) NOT NULL,
+  `category` int(11) NOT NULL,
   `price` int(24) NOT NULL,
-  `stock` int(4) NOT NULL
+  `stock` int(4) NOT NULL,
+  `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `food`
+-- Dumping data for table `food`
 --
 
-INSERT INTO `food` (`id`, `name`, `category`, `price`, `stock`) VALUES
-(5, 'Ayam Geprek', '1', 18000, 20),
-(9, 'Babi Goreng', '1', 34000, 10),
-(10, 'Nasi Goreng Babi', '1', 35000, 8),
-(11, 'Kwetiau siram sapi', '1', 35000, 35),
-(12, 'Siomay (5pcs)', '1', 25000, 12),
-(13, 'Jus Mangga', '2', 15000, 1),
-(14, 'Jus Kedondong', '2', 15000, 2);
+INSERT INTO `food` (`id`, `name`, `category`, `price`, `stock`, `gambar`) VALUES
+(15, 'Brown Sugar Fresh Milk Boba', 2, 21000, 19, 'assets/img/gambar/brown_sugar_fresh_milk_boba.jpg'),
+(16, 'Brown Sugar Boba Milk Tea', 2, 19000, 15, 'assets/img/gambar/brown_sugar_boba_milk_tea.jpg'),
+(17, 'Matcha Boba', 2, 23000, 21, 'assets/img/gambar/matcha_boba.jpg'),
+(19, 'Chocolate Chip Cookie (3 cookies)', 2, 24000, 20, 'assets/img/gambar/choco_chip_cookie.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `shop_cart`
+-- Table structure for table `shop_cart`
 --
 
 CREATE TABLE `shop_cart` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `food_id` int(11) NOT NULL,
-  `food_qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_order` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `tanggal_order` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `shop_detail`
+--
+
+CREATE TABLE `shop_detail` (
+  `id_order` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -102,18 +111,19 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `tgl_lahir`, `alamat`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
 (4, 'Gregorius Hariyanto', 'Setiadi', '2000-07-26', 'Tamci', 'hariyantosetiadi@gmail.com', 'line_73188113827642.jpg', '$2y$10$lJKebrEnm.NcvFgEl.yCMu1z1dl0.ZyXr60rbyUfu3r1fOroYFeby', 1, 1, 1589164612),
 (5, 'Kevin Natanel', 'Hendrawan', '2009-01-15', 'konoha', 'hari@student.umn.ac.id', '6722540-m.jpg', '$2y$10$VlMwwpHZLC4f8F/ngr9LhuAHJuPbcsz96tlgGta.9UbgfoliRDx9q', 2, 1, 1589169467),
-(6, 'Mickey', 'Richardo', '2020-05-05', 'amrik', 'mickey@gmail.com', '3840x2160-pebble-stone-sea-coast-4k_1551643576.jpg', '$2y$10$QR5mDO78PcNZwa43G0B8U.AHXA1EVC5mUtpiO1sTEBPDvUIsv0xai', 2, 1, 1589369717);
+(6, 'Mickey', 'Richardo', '2020-05-05', 'amrik', 'mickey@gmail.com', '3840x2160-pebble-stone-sea-coast-4k_1551643576.jpg', '$2y$10$QR5mDO78PcNZwa43G0B8U.AHXA1EVC5mUtpiO1sTEBPDvUIsv0xai', 2, 1, 1589369717),
+(7, 'Jennie', 'Florensia', '2001-01-01', 'Regensi no. 31', 'jennie@student.umn.ac.id', '70974065_528339501066978_3200452746708779008_n.jpg', '$2y$10$3/n1cRqfKaoWEsnHo8vsH.S9R8rSqZWBwku0HTNJDmUkyI49maShe', 2, 1, 1589640428);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_access_menu`
+-- Table structure for table `user_access_menu`
 --
 
 CREATE TABLE `user_access_menu` (
@@ -123,7 +133,7 @@ CREATE TABLE `user_access_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user_access_menu`
+-- Dumping data for table `user_access_menu`
 --
 
 INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
@@ -141,7 +151,7 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_menu`
+-- Table structure for table `user_menu`
 --
 
 CREATE TABLE `user_menu` (
@@ -150,7 +160,7 @@ CREATE TABLE `user_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user_menu`
+-- Dumping data for table `user_menu`
 --
 
 INSERT INTO `user_menu` (`id`, `menu`) VALUES
@@ -162,7 +172,7 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_role`
+-- Table structure for table `user_role`
 --
 
 CREATE TABLE `user_role` (
@@ -171,7 +181,7 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user_role`
+-- Dumping data for table `user_role`
 --
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
@@ -181,7 +191,7 @@ INSERT INTO `user_role` (`id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_sub_menu`
+-- Table structure for table `user_sub_menu`
 --
 
 CREATE TABLE `user_sub_menu` (
@@ -194,7 +204,7 @@ CREATE TABLE `user_sub_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user_sub_menu`
+-- Dumping data for table `user_sub_menu`
 --
 
 INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
@@ -216,104 +226,136 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 --
 
 --
--- Indeks untuk tabel `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `food`
+-- Indexes for table `food`
 --
 ALTER TABLE `food`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
 
 --
--- Indeks untuk tabel `shop_cart`
+-- Indexes for table `shop_cart`
 --
 ALTER TABLE `shop_cart`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_user` (`id_user`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `shop_detail`
+--
+ALTER TABLE `shop_detail`
+  ADD PRIMARY KEY (`id_order`,`id_menu`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_access_menu`
+-- Indexes for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_menu`
+-- Indexes for table `user_menu`
 --
 ALTER TABLE `user_menu`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_role`
+-- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_sub_menu`
+-- Indexes for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `food`
+-- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- AUTO_INCREMENT untuk tabel `shop_cart`
+-- AUTO_INCREMENT for table `shop_cart`
 --
 ALTER TABLE `shop_cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `user_access_menu`
+-- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- AUTO_INCREMENT untuk tabel `user_menu`
+-- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT untuk tabel `user_role`
+-- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `user_sub_menu`
+-- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `category` FOREIGN KEY (`category`) REFERENCES `category` (`id`);
+
+--
+-- Constraints for table `shop_cart`
+--
+ALTER TABLE `shop_cart`
+  ADD CONSTRAINT `shop_cart_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `shop_detail`
+--
+ALTER TABLE `shop_detail`
+  ADD CONSTRAINT `shop_detail_ibfk_1` FOREIGN KEY (`id_order`) REFERENCES `shop_cart` (`id_order`),
+  ADD CONSTRAINT `shop_detail_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `food` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
